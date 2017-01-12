66aeca84dc14766346db6f3845b914c3e390f195
임대현 <daihyun99@gmail.com>
2017-01-12 16:48:47 +0900

    config/autoload.php fix

--- vendor/codeigniter/framework/application/config/autoload.php
+++ vendor/codeigniter/framework/application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database', 'session', 'hotplacemaker'=>'hotpm');
+$autoload['libraries'] = array('database', 'session', 'HotPlaceMaker'=>'hotpm');
 
 /*
 | -------------------------------------------------------------------
