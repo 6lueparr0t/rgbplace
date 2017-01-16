e8b4bb218f610071a801a936f65c4690bf02207a
임대현 <daihyun99@gmail.com>
2017-01-17 03:00:17 +0900

    route modify

--- vendor/codeigniter/framework/application/config/config.php
+++ vendor/codeigniter/framework/application/config/config.php
@@ -26,8 +26,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 #$config['base_url'] = 'http://hotplacemaker.gonetis.com:50380';
 #$config['base_url'] = 'http://192.168.219.200:50380';
-$config['base_url'] = 'http://192.168.1.3:50380';
-#$config['base_url'] = '';
+#$config['base_url'] = 'http://192.168.1.3:50380';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
