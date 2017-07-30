79557b1e086cd6feb87e9418a581c84e46953a77
임대현 <daihyun99@gmail.com>
2017-07-30 21:32:23 +0900

    error fix

--- vendor/codeigniter/framework/application/libraries/RGBplace.php
+++ vendor/codeigniter/framework/application/libraries/RGBplace.php
@@ -46,13 +46,13 @@ class RGBplace {
 <html lang='en'>
 <head>
 	<meta charset='UTF-8'>
-	<meta http-equiv='X-UA-Compatible' content='IE=edge' />
-	<meta name='viewport' content='width=device-width initial-scale=1.0 maximum-scale=1.0 user-scalable=yes' />
+	<meta http-equiv='X-UA-Compatible' content='IE=edge'/>
+	<meta name='viewport' content='width=device-width initial-scale=1.0 maximum-scale=1.0 user-scalable=yes'/>
 
 	<title>RGB PLACE</title>
 
 	<link rel='icon' href='data:;base64,iVBORw0KGgo='>
-	<link rel='stylesheet' href='/assets/css/dist/style.min.css' />
+	<link rel='stylesheet' href='/assets/css/dist/style.min.css'/>
 </head>
 <body>
 		");
