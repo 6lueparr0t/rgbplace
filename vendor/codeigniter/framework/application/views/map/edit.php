7d8b6a0e06bab2de45d746b2ee2716818d712b0a
임대현 <daihyun99@gmail.com>
2018-08-10 00:46:39 +0900

    Editor Development - 3

--- vendor/codeigniter/framework/application/views/map/edit.php
+++ vendor/codeigniter/framework/application/views/map/edit.php
@@ -1,12 +1,16 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); $this->root->start(); $this->root->common();?>
-<link rel='stylesheet' href='/assets/plugin/simplemde/simplemde.min.css'/>
-<textarea id='editor'></textarea>
 
+<? echo form_open('/', ['class' => '', 'name' => '', 'id' => '']); ?>
 <!-- map/edit/<?=$map?>/<?=$type?>/<?=$num?> -->
-<script src='/assets/plugin/simplemde/simplemde.min.js'></script>
-<script>
-var simplemde = new SimpleMDE({ element: document.getElementById("editor") });
+<input type='text' id='edit-title' name='edit-title' value='' />
+<input type='hidden' id='edit-content' name='edit-content' value='' />
+<div id='edit-content-div' name='edit-content-div' contenteditable='true'>
+</div>
+<? echo form_close(); ?>
 
-simplemde.value("<?=$content?>");
-</script>
-<?php $this->root->end($path); ?>
+<? echo form_open('admin/upload', ['name' => 'upload', 'id' => 'upload']); ?>
+  <label for="input_zone" class="drop_wait"></label>
+  <input type="file" id="input_zone" name="userfile[]" multiple />
+<? echo form_close(); ?>
+
+<?php $this->root->end($path);?>
