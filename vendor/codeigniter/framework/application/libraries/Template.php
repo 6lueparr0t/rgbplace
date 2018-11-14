905735f1c86d48c9d0209160c8a4aa2a0c179d54
임대현 <daihyun99@gmail.com>
2018-11-14 00:54:27 +0000

    Update vendor/codeigniter/framework/application/libraries/Template.php

--- vendor/codeigniter/framework/application/libraries/Template.php
+++ vendor/codeigniter/framework/application/libraries/Template.php
@@ -54,15 +54,14 @@ class Template {
 		echo("<link rel='icon' href='data:;base64,iVBORw0KGgo='>");
 		echo("<link rel='stylesheet' href='/assets/css/dist/style.min.css'/>");
 
-		// Google Analytics
-		echo ('<!-- Global site tag (gtag.js) - Google Analytics -->'.
-			'<script async src="https://www.googletagmanager.com/gtag/js?id=UA-79549126-2"></script>'.
-			'<script>'.
-			'window.dataLayer = window.dataLayer || [];'.
-			'function gtag(){dataLayer.push(arguments);}'.
-			'gtag("js", new Date());'.
-			'gtag("config", "UA-79549126-2");'.
-			'</script>');
+		// Google Tag Manager
+		echo ("<!-- Google Tag Manager -->".
+			"<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':".
+			"new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],".
+			"j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=".
+			"'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);".
+			"})(window,document,'script','dataLayer','GTM-59R2NC9');</script>".
+			"<!-- End Google Tag Manager -->");
 
 		echo("</head>");
 		echo("<body>");
