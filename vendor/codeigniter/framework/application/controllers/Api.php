af2e676d1a9013f51b0a338cdea3296e9141dff9
임대현 <daihyun99@gmail.com>
2019-01-18 04:26:11 +0900

    update login error

--- vendor/codeigniter/framework/application/controllers/Api.php
+++ vendor/codeigniter/framework/application/controllers/Api.php
@@ -416,7 +416,8 @@ class Api extends CI_Controller {
 
 			$googleUser = [
 				'uid'  => $uid,
-				'name'  => $name[0].'_'.$name[1],
+				'fullname' => $name,
+				'name'  => (count($name)>1)?$name[0].'_'.$name[1]:$name[0],
 				'google' => TRUE,
 				'signed_in' => TRUE
 			];
