0c1ff5bcd9f1e24389c25a29372589f8f3d2349b
임대현 <daihyun99@gmail.com>
2019-01-18 03:05:50 +0900

    google login add - fin2

--- vendor/codeigniter/framework/assets/js/comm/src/signin/signInCheck.js
+++ vendor/codeigniter/framework/assets/js/comm/src/signin/signInCheck.js
@@ -21,7 +21,7 @@ const signOut = () => {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
         console.log('User signed out.');
-    });
+    });  
 }
 
 function signInCheck (recv) {
