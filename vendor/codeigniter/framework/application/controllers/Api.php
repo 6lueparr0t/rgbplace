15bd19f8079c406f10cc1f9779a7f18b2c5bed97
임대현 <daihyun99@gmail.com>
2019-02-19 00:52:49 +0000

    GO websocket - 3_1 (websocket_read addon)

--- vendor/codeigniter/framework/application/controllers/Api.php
+++ vendor/codeigniter/framework/application/controllers/Api.php
@@ -378,10 +378,12 @@ class Api extends CI_Controller {
 
 		$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
 		$host = $_SERVER['HTTP_HOST'];
+		$port = ':1323';
+		$path = '/push';
 
 		switch($type) {
 		case 'message' :
-			$sp = $this->websocket_open($protocol.$host.':1323/push');
+			$sp = $this->websocket_open($protocol.$host.$port.$path);
 			$msg = $data['sn'].$data['uid'];
 			$this->websocket_write($sp, $msg);
 			break;
@@ -513,12 +515,12 @@ class Api extends CI_Controller {
 	function websocket_open($url){
 		$key=base64_encode(uniqid());
 		$query=parse_url($url);
-		$header="GET /push HTTP/1.1\r\n"
+		$header="GET {$query['path']} HTTP/1.1\r\n"
 			."pragma: no-cache\r\n"
 			."cache-control: no-cache\r\n"
 			."Upgrade: WebSocket\r\n"
 			."Connection: Upgrade\r\n"
-			."Host: ".$query['host'].":".$query['port']."\r\n"
+			."Host: {$query['host']}:{$query['port']}\r\n"
 			."Sec-WebSocket-Key: $key\r\n"
 			."Sec-WebSocket-Version: 13\r\n"
 			."\r\n";
@@ -559,4 +561,96 @@ class Api extends CI_Controller {
 		return fwrite($sp,$header.$data);
 	}
 
+	/*============================================================================*\
+	Read from websocket
+	string websocket_read(resource $handle [,string &error_string])
+
+	read a chunk of data from the server, using hybi10 frame encoding
+
+	handle
+		the resource handle returned by websocket_open, if successful
+	error_string (optional)
+		A referenced variable to store error messages, i any
+	Read
+
+	Note:
+		- This implementation waits for the final chunk of data, before returning.
+		- Reading data while handling/ignoring other kind of packages
+	\*============================================================================*/
+	function _websocket_read($sp,&$error_string=NULL) {
+		$data="";
+		do{
+			// Read header
+			$header=fread($sp,2);
+			if(!$header){
+				$error_string = "Reading header from websocket failed.";
+				return false;
+			}
+			$opcode = ord($header[0]) & 0x0F;
+			$final = ord($header[0]) & 0x80;
+			$masked = ord($header[1]) & 0x80;
+			$payload_len = ord($header[1]) & 0x7F;
+
+			// Get payload length extensions
+			$ext_len = 0;
+			if($payload_len >= 0x7E){
+				$ext_len = 2;
+				if($payload_len == 0x7F) $ext_len = 8;
+				$header=fread($sp,$ext_len);
+				if(!$header){
+					$error_string = "Reading header extension from websocket failed.";
+					return false;
+				}
+
+				// Set extented paylod length
+				$payload_len= 0;
+				for($i=0;$i<$ext_len;$i++)
+					$payload_len += ord($header[$i]) << ($ext_len-$i-1)*8;
+			}
+
+			// Get Mask key
+			if($masked){
+				$mask=fread($sp,4);
+				if(!$mask){
+					$error_string = "Reading header mask from websocket failed.";
+					return false;
+				}
+			}
+
+			// Get payload
+			$frame_data='';
+			do{
+				$frame= fread($sp,$payload_len);
+				if(!$frame){
+					$error_string = "Reading from websocket failed.";
+					return false;
+				}
+				$payload_len -= strlen($frame);
+				$frame_data.=$frame;
+			}while($payload_len>0);
+			// Handle ping requests (sort of) send pong and continue to read
+			if($opcode == 9){
+				// Assamble header: FINal 0x80 | Opcode 0x0A + Mask on 0x80 with zero payload
+				fwrite($sp,chr(0x8A) . chr(0x80) . pack("N", rand(1,0x7FFFFFFF)));
+				continue;
+
+				// Close
+			} elseif($opcode == 8){
+				fclose($sp);
+
+				// 0 = continuation frame, 1 = text frame, 2 = binary frame
+			}elseif($opcode < 3){
+				// Unmask data
+				$data_len=strlen($frame_data);
+				if($masked)
+					for ($i = 0; $i < $data_len; $i++)
+						$data.= $frame_data[$i] ^ $mask[$i % 4];
+				else
+					$data.= $frame_data;
+			}else
+				continue;
+		}while(!$final);
+
+		return $data;
+	}
 }
