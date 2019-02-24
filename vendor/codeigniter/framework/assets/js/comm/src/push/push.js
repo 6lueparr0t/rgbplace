e5c34f8f5af6b37f6731213dcee33f0111115cd7
임대현 <daihyun99@gmail.com>
2019-02-24 19:36:07 +0900

    GO websocket - 8 (push done. alert func start)

---
+++ vendor/codeigniter/framework/assets/js/comm/src/push/push.js
@@ -0,0 +1,46 @@
+"use strict";
+
+var loc = window.location;
+var uri = 'ws:';
+var port = ':1323';
+
+if (loc.protocol === 'https:') {
+	uri = 'wss:';
+}
+uri += '//' + loc.hostname + port + '/push';
+
+//console.log(uri);
+
+var ws = new WebSocket(uri)
+
+ws.onopen = () => {
+	//console.log('Websocket Connected.')
+}
+
+ws.onmessage = (event) => {
+	let push = JSON.parse(event.data);
+
+	switch (push['act']) {
+		case 'alert':
+			//push['map'] push['post'] push['reply'] push['recv'] push['data']
+			switch (push['mode']) {
+				case 'uni' :
+					break;
+				case 'multi' :
+					break;
+				case 'broad' :
+					break;
+			}
+			break;
+		case 'noti':
+			switch (push['mode']) {
+				case 'count' :
+					document.getElementById('counter').innerHTML = push['data'].toLocaleString();
+					break;
+			}
+			break;
+	}
+	//var out = document.getElementById('output');
+	//out.innerHTML += event.data + '<br>';
+	//console.log(event);
+}
