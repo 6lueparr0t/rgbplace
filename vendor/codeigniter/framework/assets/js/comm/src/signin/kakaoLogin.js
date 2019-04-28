83898bea69db4aab67100cd83b7b902f6d46085f
임대현 <daihyun99@gmail.com>
2019-04-28 21:34:27 +0900

    naver login addon - fix5

--- vendor/codeigniter/framework/assets/js/comm/src/signin/kakaoLogin.js
+++ vendor/codeigniter/framework/assets/js/comm/src/signin/kakaoLogin.js
@@ -1,33 +1,35 @@
-Kakao.init('9135b26ea16bc5992bc76c59b9b32873');
 var kakao = document.getElementById('kakao');
 
-if(kakao) kakao.addEventListener("click", () => {
-	// 로그인 창을 띄웁니다.
-	Kakao.Auth.login({
-		success: function(authObj) {
-			// 로그인 성공시, API를 호출합니다.
-			Kakao.API.request({
-				url: '/v2/user/me',
-				success: function(res) {
-					let profile = res;
+if(kakao) {
+	Kakao.init('9135b26ea16bc5992bc76c59b9b32873');
+	kakao.addEventListener("click", () => {
+		// 로그인 창을 띄웁니다.
+		Kakao.Auth.login({
+			success: function(authObj) {
+				// 로그인 성공시, API를 호출합니다.
+				Kakao.API.request({
+					url: '/v2/user/me',
+					success: function(res) {
+						let profile = res;
 
-					let data = [{
-						uid : profile.id,
-						name : profile.properties.nickname
-					}];
+						let data = [{
+							uid : profile.id,
+							name : profile.properties.nickname
+						}];
 
-					httpRequest('post', '/oauth/kakao', JSON.stringify(data), data => { location.replace(document.querySelector("input[name='http_referer']").value); }, data => { console.log(data); });
-				},
-				fail: function(error) {
-					console.log(JSON.stringify(error));
-				}
-			});
-		},
-		fail: function(err) {
-			console.log(JSON.stringify(err));
-		}
+						httpRequest('post', '/oauth/kakao', JSON.stringify(data), data => { location.replace(document.querySelector("input[name='http_referer']").value); }, data => { console.log(data); });
+					},
+					fail: function(error) {
+						console.log(JSON.stringify(error));
+					}
+				});
+			},
+			fail: function(err) {
+				console.log(JSON.stringify(err));
+			}
+		});
 	});
-});
+}
 
 !(() => {
 
